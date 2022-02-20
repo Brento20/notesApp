fdc433b45656e06fff24421f23b32c8695e25ced
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-21 08:36:40 +1100

    added in forwarding routes to link index.html and notes.html

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const db = require("./db/db")
+
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
