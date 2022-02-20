c6fe37b66bbd28909407b9ec83236cc74bd42ad4
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-21 08:22:52 +1100

    fixed up a couple mistakes and got the server.js to run in console.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const app = express;
+const app = express();
 
 const path = require("path");
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 //
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
