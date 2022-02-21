ba8e1a68e34c2cf44314d409e252dfa9ef925ea9
Brenton Weaver <brenton.weaver@gmail.com>
2022-02-21 18:40:24 +1100

    testing out creating new notes

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const path = require("path");
 const db = require("./db/db")
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 
 // get requests //
 
@@ -24,6 +25,38 @@ app.route("/api/notes")
     .get(function (req, res) {
         res.json(db);
     })
+
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "../db/db.json");
+        let newNote = req.body;
+
+
+        let maxId = 20;
+
+        for (let i = 0; i < db.length; i++) {
+            let individualNote = db[i];
+            if (individualNote.id > maxId) {
+                maxId = individualNote.id;
+            }
+        }
+
+        newNote.id = maxId + 1;
+
+        db.push(newNote)
+
+
+        fs.writeFile(jsonFilePath, JSON.stringify(db), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+
+        res.json(newNote);
+    });
+
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
